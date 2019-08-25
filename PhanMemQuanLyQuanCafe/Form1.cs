b205b34262ee0a84bcd9518e6636b989d46b82d3
yukatat <yukatat@gmail.com>
2019-08-25 15:36:58 +0700

    update

--- PhanMemQuanLyQuanCafe/Form1.cs
+++ PhanMemQuanLyQuanCafe/Form1.cs
@@ -16,5 +16,10 @@ namespace PhanMemQuanLyQuanCafe
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //test123
+        }
     }
 }
